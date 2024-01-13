62d0aa5ed36ae3fc85b9e62465bda323e4d064dc
Username <dernur0505daryn@gmail.com>
2024-01-14 00:45:12 +0600

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  base: "/de-wallet/",
 
   css: {
     preprocessorOptions: {
@@ -12,4 +14,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
